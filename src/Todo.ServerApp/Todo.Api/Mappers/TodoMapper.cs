a2cc07f9490612965d9178ae86eeef26d08eee72
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2024-02-09 16:23:05 +0500

    Create todo Item dto and mapper

---
+++ src/Todo.ServerApp/Todo.Api/Mappers/TodoMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Todo.Api.Dtos;
+using Todo.Domain.Entities;
+
+namespace Todo.Api.Mappers;
+
+public class TodoMapper : Profile
+{
+    public TodoMapper()
+    {
+        CreateMap<TodoItem, TodoDto>().ReverseMap();
+    }
+}
